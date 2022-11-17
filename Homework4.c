6f6bf8bb9b57645a48b97ac77b8901a3c649c7b8
Troqneca <118365206+Troqneca@users.noreply.github.com>
2022-11-17 15:49:48 +0200

    Add files via upload

---
+++ Homework4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int num1 = -20;
+int num2 = 30;
+int num3;
+int num4;
+
+printf(" Number 1 = %d, Number 2 = %d\n", num1, num2);
+
+num3 = num1;
+num4 = num2;
+num1 = num4;
+num2 = num3;
+printf(" Number 1 = %d, Number 2 = %d\n", num1, num2);
+
+return 0;
+}
